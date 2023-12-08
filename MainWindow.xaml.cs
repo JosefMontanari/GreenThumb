d996c49b7c778c6ff9188e1006853e4e49cc84bc
JosefMontanari <josef.montanari94@gmail.com>
2023-12-08 21:28:27 +0100

    add methods

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using GreenThumb.Database;
+using GreenThumb.Models;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace GreenThumb
 {
@@ -10,6 +13,26 @@ namespace GreenThumb
         public MainWindow()
         {
             InitializeComponent();
+            ListAllPlants();
+        }
+
+        private void ListAllPlants()
+        {
+            lstPlants.Items.Clear();
+            using (AppDbContext context = new())
+            {
+                UnitOfWork uow = new(context);
+                var plants = uow.PlantRepository.GetAllPlants();
+
+                foreach (var plant in plants)
+                {
+                    ListViewItem listViewItem = new ListViewItem();
+                    listViewItem.Tag = plant;
+                    listViewItem.Content = plant.Name;
+
+                    lstPlants.Items.Add(listViewItem);
+                }
+            }
         }
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
@@ -29,7 +52,33 @@ namespace GreenThumb
 
         private void btnRemove_Click(object sender, RoutedEventArgs e)
         {
+            using (AppDbContext context = new())
+            {
+                ListViewItem? item = new ListViewItem();
+                item = (ListViewItem?)lstPlants.SelectedItem;
+                if (item != null)
+                {
+                    PlantModel? plantToRemove = (PlantModel)item.Tag;
+                    var dialogResult = MessageBox.Show($"Are you sure you want to remove {plantToRemove.Name}?", "Warning", MessageBoxButton.YesNo);
+                    if (dialogResult == MessageBoxResult.Yes)
+                    {
+                        UnitOfWork uow = new(context);
 
+                        uow.PlantRepository.RemovePlant(plantToRemove.PlantId);
+                        uow.SaveChanges();
+
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("You must select a plant to remove!", "Error", MessageBoxButton.OK);
+                }
+
+
+
+                ListAllPlants();
+            }
         }
     }
 }
