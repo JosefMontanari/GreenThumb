940618d7f717b8d350f59fc8884f1d5881b3df0c
JosefMontanari <josef.montanari94@gmail.com>
2023-12-08 20:23:09 +0100

    Add InstrucitonRepo

--- Database/InstructionRepository.cs
+++ Database/InstructionRepository.cs
@@ -1,6 +1,32 @@
-﻿namespace GreenThumb.Database
+﻿using GreenThumb.Models;
+
+namespace GreenThumb.Database
 {
     public class InstructionRepository
     {
+        private readonly AppDbContext _context;
+        public InstructionRepository(AppDbContext context, PlantModel plant)
+        {
+            _context = context;
+        }
+
+        public List<InstructionModel> GetAllInstructions(PlantModel plant)
+        {
+            return _context.Instructions.Where(p => p.PlantId == plant.PlantId).ToList();
+        }
+
+        public void AddInstruction(InstructionModel instruction)
+        {
+            _context.Instructions.Add(instruction);
+        }
+
+        public void RemoveInstruction(int id)
+        {
+            InstructionModel? instructionToRemove = _context.Instructions.FirstOrDefault(p => p.InstructionId == id);
+            if (instructionToRemove != null)
+            {
+                _context.Instructions.Remove(instructionToRemove);
+            }
+        }
     }
 }
