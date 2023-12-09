7616a7d78a30c7b7557da69adc6cbb83625fa76a
JosefMontanari <josef.montanari94@gmail.com>
2023-12-10 00:34:59 +0100

    made addplantwindow

--- AddPlantWindow.xaml.cs
+++ AddPlantWindow.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GreenThumb.Models;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace GreenThumb
 {
@@ -19,9 +9,43 @@ namespace GreenThumb
     /// </summary>
     public partial class AddPlantWindow : Window
     {
+        public List<InstructionModel> instructions = new();
         public AddPlantWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnAddInstruction_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtInstruction.Text))
+            {
+                // Make the instruction and add it to the list
+                InstructionModel model = new InstructionModel();
+                model.Instruction = txtInstruction.Text;
+                instructions.Add(model);
+
+                // Add it to the listview
+                ListViewItem item = new ListViewItem();
+                item.Tag = model;
+                item.Content = model.Instruction;
+                lstInstructions.Items.Add(item);
+            }
+            else
+            {
+                MessageBox.Show("You need to write an instruction!");
+            }
+        }
+
+        private void btnAddPlant_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            Close();
         }
     }
 }
