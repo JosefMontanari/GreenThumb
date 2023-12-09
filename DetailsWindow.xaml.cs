4d291f7661754a29f32eb53ac69de95295e921a1
JosefMontanari <josef.montanari94@gmail.com>
2023-12-09 15:03:40 +0100

    Begin add plant window

--- DetailsWindow.xaml.cs
+++ DetailsWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using GreenThumb.Models;
+﻿using GreenThumb.Database;
+using GreenThumb.Models;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace GreenThumb
 {
@@ -11,6 +13,31 @@ namespace GreenThumb
         public DetailsWindow(PlantModel plant)
         {
             InitializeComponent();
+
+            lblPlantName.Content = plant.Name;
+
+            txtDetails.Text = plant.Description;
+
+            using (AppDbContext context = new())
+            {
+                var instructions = context.Instructions.Where(p => p.PlantId == plant.PlantId).ToList();
+
+                foreach (var instruction in instructions)
+                {
+                    ListViewItem item = new ListViewItem();
+                    item.Tag = instruction;
+                    item.Content = instruction.Instruction;
+
+                    lstInstructions.Items.Add(item);
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            Close();
         }
     }
 }
